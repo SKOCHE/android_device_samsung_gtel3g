8cb4baa543ddb4ce6acee74a7c327061dd54d5dd
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2025-06-09 16:35:09 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,7 +11,11 @@ ALLOW_MISSING_DEPENDENCIES := true
 # Architecture
 TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 :=armeabi
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := recovery
